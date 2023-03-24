1c1ef29e7bf5f95d74b99568483fee2e65a44f19
Alexander Voronov <avv.0@ya.ru>
2023-03-25 00:26:11 +0300

    LibreVNA-GUI:librevnatcpdriver: Convert QString to QByteArray

--- Software/PC_Application/LibreVNA-GUI/Device/LibreVNA/librevnatcpdriver.cpp
+++ Software/PC_Application/LibreVNA-GUI/Device/LibreVNA/librevnatcpdriver.cpp
@@ -66,7 +66,7 @@ std::set<QString> LibreVNATCPDriver::GetAvailableDevices()
                 "MAN: \"ssdp:discover\"\r\n"
                 "MX: 1\r\n"
                 "ST: ");
-    data.append(service_name);
+    data.append(service_name.toUtf8());
     data.append("\r\n"
                 "\r\n");
 
