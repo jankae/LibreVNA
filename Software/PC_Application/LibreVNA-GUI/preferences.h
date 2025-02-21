d722f629478ddafd616acb1cb2fa6120e9e88b87
Jan Käberich <j.kaeberich@gmx.de>
2025-02-21 15:59:56 +0100

    dwell time defaults to 0

--- Software/PC_Application/LibreVNA-GUI/preferences.h
+++ Software/PC_Application/LibreVNA-GUI/preferences.h
@@ -233,7 +233,7 @@ private:
         {&Startup.DefaultSweep.points, "Startup.DefaultSweep.points", 501},
         {&Startup.DefaultSweep.bandwidth, "Startup.DefaultSweep.bandwidth", 1000.0},
         {&Startup.DefaultSweep.averaging, "Startup.DefaultSweep.averaging", 1},
-        {&Startup.DefaultSweep.dwellTime, "Startup.DefaultSweep.dwellTime", 60e-6},
+        {&Startup.DefaultSweep.dwellTime, "Startup.DefaultSweep.dwellTime", 0.0},
         {&Startup.Generator.frequency, "Startup.Generator.frequency", 1000000000.0},
         {&Startup.Generator.level, "Startup.Generator.level", -10.00},
         {&Startup.SA.start, "Startup.SA.start", 950000000.0},
