b5da8015b55ce5721d102fc9a1732de38cd8ea9e
Jan Käberich <j.kaeberich@gmx.de>
2025-05-31 15:14:19 +0200

    HIL: more rigorous test of firmware update process

--- Software/Integrationtests/tests/TestUpdate.py
+++ Software/Integrationtests/tests/TestUpdate.py
@@ -4,8 +4,11 @@ import subprocess
 
 class TestUpdate(TestBase):
     def test_Update(self):
+        # first update: actually update to the firmware version we want to test
         self.vna.cmd("DEV:UPDATE ../../combined.vnafw", timeout=60)
-
+        # second update: check that we still have a working firmware update with this version
+        self.vna.cmd("DEV:UPDATE ../../combined.vnafw", timeout=60)
+        
         reported = self.vna.query("DEV:INF:FWREVISION?")
         major = subprocess.check_output("grep -oP '(?<=FW_MAJOR=)[0-9]+' ../VNA_embedded/Makefile", shell=True).strip()
         minor = subprocess.check_output("grep -oP '(?<=FW_MINOR=)[0-9]+' ../VNA_embedded/Makefile", shell=True).strip()
